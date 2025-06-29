43ca74abde72e94ee36da9211e7bf2eb0b826a89
Zickles <zicklesistaken@gmail.com>
2025-06-29 13:09:57 -0400

    clean: imports

--- src/main/kotlin/me/odinmod/odin/events/MessageSentEvent.kt
+++ src/main/kotlin/me/odinmod/odin/events/MessageSentEvent.kt
@@ -1,6 +1,5 @@
 package me.odinmod.odin.events
 
 import me.odinmod.odin.events.core.CancellableEvent
-import net.minecraft.client.util.InputUtil
 
 class MessageSentEvent(val message: String): CancellableEvent()
