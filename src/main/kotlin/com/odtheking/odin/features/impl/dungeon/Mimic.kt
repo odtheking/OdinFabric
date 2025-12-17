ad409b341383477480fbd1759bd05b1c98c80c84
Starred <79894632+skies-starred@users.noreply.github.com>
2025-12-17 20:29:43 +0530

    fixes for Mimic module (#30)
    * fix(Mimic): mimic kill being detected on berserk ultimate
    
    * add message about Mimic module in Map Info description

--- src/main/kotlin/com/odtheking/odin/features/impl/dungeon/Mimic.kt
+++ src/main/kotlin/com/odtheking/odin/features/impl/dungeon/Mimic.kt
@@ -13,6 +13,7 @@ import com.odtheking.odin.utils.skyblock.dungeon.DungeonListener
 import com.odtheking.odin.utils.skyblock.dungeon.DungeonUtils
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket
+import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.monster.Zombie
 
 object Mimic : Module(
@@ -33,16 +34,23 @@ object Mimic : Module(
         onReceive<ClientboundRemoveEntitiesPacket> {
             if (!DungeonUtils.isFloor(6, 7) || DungeonUtils.inBoss || DungeonUtils.mimicKilled) return@onReceive
             entityIds.forEach { id ->
-                val entity = mc.level?.getEntity(id) ?: return@forEach
-                if (entity is Zombie && entity.isBaby) mimicKilled()
+                val entity = mc.level?.getEntity(id) as? Zombie ?: return@forEach
+                if (!entity.isBaby) return@forEach
+                if (entity.hasItemInSlot(EquipmentSlot.HEAD)) return@onReceive
+
+                mimicKilled()
             }
         }
 
         onReceive<ClientboundSetEntityDataPacket> {
             if (!DungeonUtils.isFloor(6, 7) || DungeonUtils.inBoss || DungeonUtils.mimicKilled) return@onReceive
-            val entity = runCatching { mc.level?.getEntity(id) }.getOrNull() ?: return@onReceive
+            val entity = runCatching { mc.level?.getEntity(id) }.getOrNull() as? Zombie ?: return@onReceive
             val health = (packedItems.find { it.id == 9 }?.value as? Float) ?: return@onReceive
-            if (entity is Zombie && entity.isBaby && health <= 0f) mimicKilled()
+            if (!entity.isBaby) return@onReceive
+            if (entity.hasItemInSlot(EquipmentSlot.HEAD)) return@onReceive
+            if (health > 0f) return@onReceive
+
+            mimicKilled()
         }
 
         on<ChatPacketEvent> {
