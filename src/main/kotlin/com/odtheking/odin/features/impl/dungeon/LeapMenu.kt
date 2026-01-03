4feb034188bf970be59bc3cc205109b7e28922fa
shaweel <152102143+shaweel@users.noreply.github.com>
2026-01-03 16:31:00 +0100

    Include class in leap announcements

--- src/main/kotlin/com/odtheking/odin/features/impl/dungeon/LeapMenu.kt
+++ src/main/kotlin/com/odtheking/odin/features/impl/dungeon/LeapMenu.kt
@@ -128,7 +128,14 @@ object LeapMenu : Module(
 
         on<ChatPacketEvent> {
             if (!leapAnnounce || !DungeonUtils.inDungeons) return@on
-            leapedRegex.find(value)?.groupValues?.get(1)?.let { sendCommand("pc Leaped to ${it}!") }
+            leapedRegex.find(value)?.groupValues?.get(1)?.let { username ->
+                val clazz = DungeonUtils.leapTeammates
+                    .firstOrNull { it.name.equals(username.noControlCodes, ignoreCase = true) }
+                    ?.clazz ?: DungeonClass.Unknown
+        
+                val classSuffix = if (clazz != DungeonClass.Unknown) " (${clazz.name})" else ""
+                sendCommand("pc Leaped to ${username}${classSuffix}!")
+            }
         }
     }
 
