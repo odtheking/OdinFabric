6ceeb70950e23b127621df3885257ba98165b4c5
odtheking <odtheking1@gmail.com>
2026-01-03 11:14:27 +0200

    Fixed AutoSprint only working if keybind was set

---
+++ src/main/java/com/odtheking/mixin/mixins/LocalPlayerMixin.java
@@ -0,0 +1,22 @@
+package com.odtheking.mixin.mixins;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.odtheking.odin.features.impl.skyblock.AutoSprint;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LocalPlayer.class)
+public abstract class LocalPlayerMixin {
+
+    @ModifyExpressionValue(
+            method = "aiStep",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Input;sprint()Z"
+            )
+    )
+    private boolean odin$autoSprint(boolean original) {
+        return original || AutoSprint.INSTANCE.getEnabled();
+    }
+}
--- src/main/kotlin/com/odtheking/odin/features/impl/skyblock/AutoSprint.kt
+++ src/main/kotlin/com/odtheking/odin/features/impl/skyblock/AutoSprint.kt
@@ -1,19 +1,8 @@
 package com.odtheking.odin.features.impl.skyblock
 
-import com.odtheking.mixin.accessors.KeyMappingAccessor
-import com.odtheking.odin.events.TickEvent
-import com.odtheking.odin.events.core.on
 import com.odtheking.odin.features.Module
-import net.minecraft.client.KeyMapping
 
 object AutoSprint : Module(
     name = "Auto Sprint",
     description = "Automatically makes you sprint."
-) {
-
-    init {
-        on<TickEvent.End> {
-            KeyMapping.set((mc.options.keySprint as KeyMappingAccessor).boundKey, true)
-        }
-    }
-}
+)
